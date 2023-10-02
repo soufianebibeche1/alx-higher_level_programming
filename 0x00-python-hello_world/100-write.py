8981fec988b2b0da4d845e5d19492313462133c1
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-10-02 16:58:52 -0700

    teste

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 import sys
-msg = "and that piece of art is useful - Dora Korpar, 2015-10-19 \n"
+msg = "and that piece of art is useful - Dora Korpar, 2015-10-19\n" 
 sys.stderr.write(msg)
 sys.exit(1)
