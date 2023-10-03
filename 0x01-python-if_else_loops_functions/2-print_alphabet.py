2be298dfeb36b2093b085430fdc90ef232d861d0
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-10-03 12:17:02 -0700

    test

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for character in range(ord("a"), ord("z") + 1):
-    print(chr(character), end="")
+
+for character in range(ord("a"),ord("z")+1):
+    print("{}".format(chr(character)), end="")
