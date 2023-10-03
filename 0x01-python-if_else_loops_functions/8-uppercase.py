57339c0c203b66483ce122d24fae93fcd79d5342
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-10-03 16:50:18 -0700

    tss

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -6,7 +6,8 @@ def islower(c):
     else:
         return False
 
+
 def uppercase(str):
     for char in str:
-        print("{:c}".format(ord(char) if not islower(char) else ord(char) - 32), end="")
+        print("{:c}".format(ord(char)if not islower(char) else ord(char) - 32), end="")
     print("")
