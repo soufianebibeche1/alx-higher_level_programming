3452e337044804967d065b599367cf8650c47bc4
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-10-07 16:53:33 -0700

    test

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-
     if not sentence:
-        return(0, None)
+        return (0, None)
 
     length = len(sentence)
     first_char = sentence[0]
-    return(length, first_char)
+
+    return (length, first_char)
