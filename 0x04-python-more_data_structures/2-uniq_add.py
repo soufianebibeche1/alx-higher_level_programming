1b32eb9cb7ac989ef86b6f5c8d8b80f52590d891
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-10-11 14:42:39 -0700

    test

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     uniq_val = set()
-    result = sum(map(lambda x: uniq_val.add(x) or x if x not in uniq_val else 0, my_list))
+    result = sum(
+        map(
+            lambda x: uniq_val.add(x) or x if x not in uniq_val else 0,
+            my_list))
     return result
