0b9bfd130699ecec79e285d101045e43cbaf47cf
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-10-23 14:27:44 -0700

    add 0 correctio

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,8 +7,7 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
             cpt += 1
         print()
-
-    except:
+    except IndexError:
         pass
 
     return cpt
