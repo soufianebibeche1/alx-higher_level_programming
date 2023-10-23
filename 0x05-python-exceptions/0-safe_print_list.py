3f51885f62920cad65132daa22aa69aad36308dd
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-10-23 14:34:28 -0700

    test

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,12 +2,12 @@
 
 def safe_print_list(my_list=[], x=0):
     cpt = 0
-    try:
-        for i in range(x):
-            print(my_list[i], end="")
+    for i in range(x):
+        try:
+            print(f"{my_list[i]}", end="")
             cpt += 1
-        print()
-    except IndexError:
-        pass
 
+        except IndexError:
+            break
+        print("")
     return cpt
