41feca483c67cb426c0bf1ef1a4790f42d08e6a3
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-10-30 12:57:08 -0700

    test 2

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# 1-rectangle.py
 """ CLASS Rectangle """
 
 
@@ -6,11 +7,13 @@ class Rectangle:
     """ CLASS Definition """
 
     def __init__(self, width=0, height=0):
+        """ Constructor """
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """Get/set the width of the rectangle."""
         return self.__width
 
     @width.setter
@@ -23,6 +26,7 @@ class Rectangle:
 
     @property
     def height(self):
+        """Get/set the height of the rectangle."""
         return self.__height
 
     @height.setter
