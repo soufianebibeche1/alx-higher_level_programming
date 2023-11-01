ee16876682ead222309ef3ae2a8ad5ebf80512ec
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-11-01 15:01:22 -0700

    test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
+""" add inetegers """
 
 def add_integer(a, b=98):
-    """
-    Adds two integers.
+    """ Adds two integers."""
 
-    Args:
-        a (int or float): The first number.
-        b (int or float, optional): The second number (default is 98).
-
-    Returns:
-        int: The addition of a and b as integers.
-
-    Raises:
-        TypeError: If a or b is not an integer or float.
-
-    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
