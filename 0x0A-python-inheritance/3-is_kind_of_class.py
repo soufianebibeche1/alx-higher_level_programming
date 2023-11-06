bb60bbf5286468e92e231655a91937cf1ce92cc7
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-11-06 22:54:48 +0000

    test

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 3-is_kind_of_class.py
+"""Defines a class-checking function."""
+
+
+def is_kind_of_class(obj, a_class):
+    """Returns True if obj's class is the same as or inherits from a_class; otherwise False."""
+    if isinstance(obj,a_class):
+        return True
+    return False
