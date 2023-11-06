4081d12a34a7975c036b717e9623b96317becfbd
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-11-06 22:26:23 +0000

    testem

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 # 1-my_list.py:wq
-""" Class MyList"""
+"""Defines an inherited list class MyList."""
 
 
 class MyList(list):
-    """ Class MyList """
+    """Implements sorted printing for the built-in list class."""
+
     def print_sorted(self):
         """A list class that overrides some of the built-in methods."""
         print(sorted(self))
