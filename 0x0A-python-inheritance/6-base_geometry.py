4d8215da00007fc5345fd3bf7b6970b544db6f36
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-11-06 23:18:11 +0000

    test

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 6-base_geometry.py
+"""Defines a class-BaseGeometry."""
+
+
+class BaseGeometry:
+    """A base geometry class for all geometrical objects in the program."""
+    def area(self):
+        """ raise an exception """
+        raise Exception("area() is not implemented")
