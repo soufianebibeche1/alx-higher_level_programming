5d6580e6c59ae84bc6f35b1d3108cadeb5e16750
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-11-06 21:02:26 +0000

    test 1 task

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # 0-lookup.py
+""" returns the list of available attributes and methods of an object """
 
 def lookup(obj):
     """ returns the list of available attributes and methods of an object """
