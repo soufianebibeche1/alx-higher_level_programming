3a1d7c78178293f752d623009c44cb5acaca22ba
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-11-07 22:02:40 +0000

    test

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,15 +4,6 @@
 
 
 def write_file(filename="", text=""):
-    """Write a string to a text file (UTF8) and return the number of characters written.
-
-    Args:
-        filename (str): The name of the file to write to.
-        text (str): The string to write to the file.
-
-    Returns:
-        int: The number of characters written to the file.
-    """
+    """Write a string to a text file (UTF8) and return the number of characters written."""
     with open(filename, mode='w', encoding='utf-8') as file:
-        nb_chars = file.write(text)
-    return nb_chars
+        return file.write(text)
