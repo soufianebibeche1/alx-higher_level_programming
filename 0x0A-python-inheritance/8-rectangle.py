53b96edc018a6632e532adcd2378275830247b24
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-11-07 00:08:44 +0000

    test

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 7-base_geometry.py
+"""Defines a class-BaseGeometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class with length and width attributes."""
+    def __init__(self, width, height):
+        """ Intialize a new Rectangle. """
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
+        self.__width = width
+        self.__height = height
