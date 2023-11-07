311b7a18d0b073a09b36f51cdae7df857eb99a7e
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-11-07 23:55:08 +0000

    test

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -14,7 +14,6 @@ class Student:
 
     def to_json(self):
         """Retrieve a dictionary representation of a Student"""
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
+        if (type(attrs) == list and all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
