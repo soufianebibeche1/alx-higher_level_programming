53e7dbdd73874c0ff65b91f4a3fa8bab74f1ec38
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-11-07 23:49:58 +0000

    test

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -9,7 +9,7 @@ class Student:
     def __init__(self, first_name, last_name, age):
         """Initialize a Student instance with first_name, last_name, and age."""
         self.first_name = first_name
-        self.lastname = last_name
+        self.last_name = last_name
         self.age = age
 
     def to_json(self):
