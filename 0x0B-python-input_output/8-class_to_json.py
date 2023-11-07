8bc511a3eacce880ff167d773023ed6144070403
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-11-07 23:08:40 +0000

    test

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 # 8-class_to_json.py
-""" Python"""
-import sys
+""" Python """
 
 
 def class_to_json(obj):
-    """ Convert a class to JSON format."""
+    """Return the dictionary represntation of a simple data structure."""
     return obj.__dict__
