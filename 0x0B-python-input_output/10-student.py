937e5e7f910e5e5b3b6e81c36cc1dab8440de994
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-11-07 23:54:13 +0000

    test

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# 9-student.py
+""" Python """
+
+
+class Student:
+    """ class Student """
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieve a dictionary representation of a Student"""
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
