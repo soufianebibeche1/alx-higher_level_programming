2ba0d21040e0ae4c4f330f00d31f7aa45012499e
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-11-07 23:51:21 +0000

    test

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -7,11 +7,11 @@ class Student:
     """ class Student """
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a Student instance with first_name, last_name, and age."""
+        """Initialize a Student instance"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """Retrieve a dictionary representation of a Student instance."""
+        """Retrieve a dictionary representation of a Student"""
         return self.__dict__
