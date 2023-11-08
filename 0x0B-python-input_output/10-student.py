bf190f24b5a7ce4429254c55b31a4da487f2747e
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-11-08 00:00:59 +0000

    test

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -13,12 +13,7 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student.
-        If attrs is a list of strings, represents only those attributes
-        included in the list.
-        Args:
-            attrs (list): (Optional) The attributes to represent.
-        """
+        """Get a dictionary representation of the Student."""
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
