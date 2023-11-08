1c73873c98223f9ef5569b69b692b1d230341ae8
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-11-08 01:05:33 +0000

    test

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 # 1-my_list.py
-"""an inherited list class MyList."""
+"""inherited list class MyList."""
 
 
 class MyList(list):
-    """Implements sorted print for built-in list."""
+    """Implements sorted print for the list class."""
 
     def print_sorted(self):
         """Print a list in sorted ascending order."""
