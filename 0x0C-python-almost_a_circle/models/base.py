69e6b0660ce3bb898d5fee955085050ea2b88d03
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-11-13 23:05:19 +0000

    test

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 # models/base.py
 """ Python: 0x0C. Python - Almost a circle """
-
-
 import json
 
 
@@ -10,6 +8,7 @@ class Base:
     """Base class for managing id attribute."""
 
     __nb_objects = 0
+
     def __init__(self, id=None):
         """ Constructor for Base class. """
         if id is not None:
