cc55cd66fb46bf427664dc9c8cb56cf8097e935f
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-11-14 00:40:47 +0000

    test

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,7 +5,7 @@ import json
 
 
 class Base:
-    """Base class for managing id attribute."""
+    """Base class for."""
 
     __nb_objects = 0
 
@@ -23,3 +23,32 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the JSON string of list_objs to a file."""
+        filename = cls.__name__ + ".json"
+        with open(filename, mode='w', encoding='utf-8') as file:
+            if list_objs is None:
+                file.write("[]")
+            else:
+                file.write(json_str)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the list represented by json_string."""
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Return an instance with all attributes already set."""
+        if cls.__name__ == "Rectangle":
+            instance = cls(1, 1)
+        elif cls.__name__ == "Square":
+            instance = cls(1)
+        else:
+            instance = None
+        instance.update(**dictionary)
+        return instance
