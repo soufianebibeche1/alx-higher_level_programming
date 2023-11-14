53c239eeff59d8740aabc9decc13e04a4785606b
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-11-14 00:22:00 +0000

    test

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,5 @@
-#!/usr/bin/python3
-# models/base.py
-""" Python: 0x0C. Python - Almost a circle """
+/base.py
+"""Module defining the Base class."""
 import json
 
 
@@ -16,3 +15,40 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON string of list_dictionaries."""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the JSON string of list_objs to a file."""
+        filename = cls.__name__ + ".json"
+        with open(filename, mode='w', encoding='utf-8') as file:
+            if list_objs is None:
+                file.write("[]")
+            else:
+                json_str = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
+                file.write(json_str)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the list represented by json_string."""
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Return an instance with all attributes already set."""
+        if cls.__name__ == "Rectangle":
+            instance = cls(1, 1)
+        elif cls.__name__ == "Square":
+            instance = cls(1)
+        else:
+            instance = None
+        instance.update(**dictionary)
+        return instance
