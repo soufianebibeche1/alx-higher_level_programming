0bee6b2c841c8cce2b5c74ed635d92eb19280d5a
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2023-12-13 14:13:28 -0800

    ttesting finished tasks

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 -- List all Comedy shows
-SELECT tv_shows.title
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+SELECT title FROM tv_shows
+JOIN tv_show_genres ON id=tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
 WHERE tv_genres.name = 'Comedy'
-ORDER BY tv_shows.title;
+ORDER BY title;
