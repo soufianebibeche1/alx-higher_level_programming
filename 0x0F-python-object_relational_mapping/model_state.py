98a4b03305472a62c94d9c9e185fd7a1273f50e9
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2024-01-13 09:09:25 -0800

    adding a new class defintions

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
-"""Start link class to table in database
-"""
-import sys
-from sqlalchemy import Column, Integer, String, create_engine
+""" CLASS DEF """
+
+
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+
 
 Base = declarative_base()
 
 
 class State(Base):
     """Class definition of a State"""
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True,
-                nullable=False, unique=True, autoincrement=True)
+
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
